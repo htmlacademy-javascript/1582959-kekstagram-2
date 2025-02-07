338549c339ebf5737c0e643d464a14bb14ae8d19
Keks <keks@htmlacademy.ru>
2025-02-07 13:11:08 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/scale.js
@@ -0,0 +1,35 @@
+const SCALE_CONTROL_STEP = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlValue = document.querySelector('.scale__control--value');
+const imagePreview = document.querySelector('.img-upload__preview img');
+
+// Функция для установки нового значения масштаба
+function setScale(newValue) {
+  if (newValue >= MIN_SCALE && newValue <= MAX_SCALE) {
+    scaleControlValue.value = `${newValue}%`;
+    imagePreview.style.transform = `scale(${newValue / 100})`;
+  }
+}
+
+const onScaleControlSmallerClick = () => {
+  const currentValue = parseInt(scaleControlValue.value, 10);
+  setScale(currentValue - SCALE_CONTROL_STEP);
+};
+
+const onScaleControlBiggerClick = () => {
+  const currentValue = parseInt(scaleControlValue.value, 10);
+  setScale(currentValue + SCALE_CONTROL_STEP);
+};
+
+scaleControlSmaller.addEventListener('click', onScaleControlSmallerClick);
+scaleControlBigger.addEventListener('click', onScaleControlBiggerClick);
+
+const resetImagePreviewScale = () => {
+  setScale(MAX_SCALE);
+};
+
+export { resetImagePreviewScale };
